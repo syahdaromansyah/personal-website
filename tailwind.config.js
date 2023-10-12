dff9e137119179e2c5589b12505096de017f65cb
Syahda Romansyah <syahdarom@gmail.com>
2023-10-12 22:45:34 +0700

    chore(config-file): extend the Tailwind transition property

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
+      transitionProperty: {
+        custom: `max-height, max-width, border-radius, ${twDefaultTheme.transitionProperty.DEFAULT}`,
+      },
       fontFamily: {
         inter: ['var(--font-inter)', ...twDefaultTheme.fontFamily.sans],
         poppins: ['var(--font-poppins)', ...twDefaultTheme.fontFamily.sans],
